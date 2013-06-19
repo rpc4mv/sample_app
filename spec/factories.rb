df24d2786e209081a549d09c0d05134917d21c7c
Ruturaj Choudhari <rpc4mv@virginia.edu>
2013-06-19 09:41:04 +0000

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name       "Ruturaj Choudhari"
+    email      "rpc4mv@virginia.edu"
+    password   "foobar"
+    password_confirmation "foobar"
+  end
+end
