36e8b5199a2232b105fe7888ed425349f13aae06
Ruturaj Choudhari <rpc4mv@virginia.edu>
2013-06-20 07:30:38 +0000

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,8 +13,13 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(name: "Example User", email: "user@example.com",
-                            password: "foobar", password_confirmation: "foobar" )}
+  before do
+     @user = User.new(name: "Example User", email: "user@example.com",
+                       password: "foobar", password_confirmation: "foobar" )
+  end
+
+  
+
                        
   subject { @user }
 
@@ -23,6 +28,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -106,4 +113,10 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -37,6 +37,15 @@ describe "UserPages" do
         it { should have_selector('title', text: 'Sign up') }
         it { should have_content('error') }
       end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
     end
 
     describe "with valid information" do
